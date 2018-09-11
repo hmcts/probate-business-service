a94879ca4bd5654ae3ca5cb19f0316f788d6e35a
praveenr <praveen.moj@gmail.com>
2018-09-11 22:00:29 +0100

    fixed error

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,5 +1,5 @@
 // Infrastructural variables
-variable "product" {}  /get from jenkins file
+variable "product" {}  //get from jenkins file
 
 variable "raw_product" {
   default = "probate" // jenkins-library overrides product for PRs and adds e.g. pr-118-cmc
