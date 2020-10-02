93669c6770369feaf266e881238e98081e312ebe
Linus Norton <linusnorton@gmail.com>
2020-10-02 10:14:29 +0100

    terraform update

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,10 +1,6 @@
 // Infrastructural variables
 variable "product" {}  //get from jenkins file
 
-variable "raw_product" {
-  default = "probate" // jenkins-library overrides product for PRs and adds e.g. pr-118-cmc
-}
-
 variable "env" {
   type = "string"
 }
