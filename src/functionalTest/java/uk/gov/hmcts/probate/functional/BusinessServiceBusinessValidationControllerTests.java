18251ea87b7230487cb62105680901798d90d38a
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2020-11-25 14:21:37 +0000

    DTSPB-811: solving test issues2

--- src/functionalTest/java/uk/gov/hmcts/probate/functional/BusinessServiceBusinessValidationControllerTests.java
+++ src/functionalTest/java/uk/gov/hmcts/probate/functional/BusinessServiceBusinessValidationControllerTests.java
@@ -1,16 +1,14 @@
 package uk.gov.hmcts.probate.functional;
 
-import io.restassured.response.Response;
-import lombok.extern.slf4j.Slf4j;
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
-@Slf4j
 @RunWith(SpringIntegrationSerenityRunner.class)
 public class BusinessServiceBusinessValidationControllerTests extends IntegrationTestBase {
 
@@ -30,23 +28,23 @@ public class BusinessServiceBusinessValidationControllerTests extends Integratio
 
     private void validateSuccess(String sessionId, String jsonFileName) {
         RestAssured.given().relaxedHTTPSValidation()
-                .headers(utils.getHeaders(sessionId))
-                .body(utils.getJsonFromFile(jsonFileName))
-                .when().post(businessServiceUrl + "/validate")
-                .then().assertThat().statusCode(200);
+            .headers(utils.getHeaders(sessionId))
+            .body(utils.getJsonFromFile(jsonFileName))
+            .when().post(businessServiceUrl + "/validate")
+            .then().assertThat().statusCode(200);
     }
 
     private void validateFailure(String jsonFileName, int errorCode, String errorMsg) {
         Response response = RestAssured.given().relaxedHTTPSValidation()
-                .headers(utils.getHeaders(SESSION_ID))
-                .body(utils.getJsonFromFile(jsonFileName))
-                .when().post(businessServiceUrl + "/validate")
-                .thenReturn();
+            .headers(utils.getHeaders(SESSION_ID))
+            .body(utils.getJsonFromFile(jsonFileName))
+            .when().post(businessServiceUrl + "/validate")
+            .thenReturn();
 
-        log.info("XXXXXX"+response.getBody().asString());
+        System.out.println("XXXXXX" + response.getBody().asString());
         response.then().assertThat().statusCode(errorCode)
-                .and().body("error", equalTo("Bad Request"))
-                .and().body("message", containsString(errorMsg));
+            .and().body("error", equalTo("Bad Request"))
+            .and().body("message", containsString(errorMsg));
     }
 
 }
