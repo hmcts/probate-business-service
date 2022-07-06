257f2ae4ba067bbde699b892317fce145b8e1d19
Iswarya Pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2022-07-06 11:29:24 +0100

    Removed unused exception class

--- src/main/java/uk/gov/hmcts/probate/services/businessdocuments/exceptions/BusinessDocumentException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.probate.services.businessdocuments.exceptions;
-
-public class BusinessDocumentException extends RuntimeException {
-
-    public BusinessDocumentException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
