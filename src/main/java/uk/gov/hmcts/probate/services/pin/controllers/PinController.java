7f7a72a4d0be9fe0f14b589d1af4037e9d1ed55b
FeliTam <feli.tam@justice.gov.uk>
2023-09-11 13:32:44 +0100

    DTSPB-3430 spring boot3

--- src/main/java/uk/gov/hmcts/probate/services/pin/controllers/PinController.java
+++ src/main/java/uk/gov/hmcts/probate/services/pin/controllers/PinController.java
@@ -1,15 +1,13 @@
 package uk.gov.hmcts.probate.services.pin.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.probate.services.pin.PinService;
@@ -19,51 +17,58 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @RestController
 @Tag(name = "Pin Service")
 public class PinController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PinController.class);
     private static final String VALID_PHONENUMBER_CHARACTERS_REGEX = "(\\+)*[0-9]+";
     private static final String INVALID_PHONENUMBER_CHARACTERS_REGEX = "[ \\(\\)\\[\\]-]";
 
     @Autowired
     private PinService pinService;
 
-    @RequestMapping(path = "/pin", method = RequestMethod.GET)
-    public ResponseEntity<String> invite(@RequestParam String phoneNumber,
+    @GetMapping(path = "/pin")
+    public ResponseEntity<String> invite(@RequestParam(value = "phoneNumber") String phoneNumber,
                                          @RequestHeader("Session-Id") String sessionId)
-        throws NotificationClientException, UnsupportedEncodingException {
+        throws UnsupportedEncodingException {
         return getStringResponseEntity(phoneNumber, sessionId, Boolean.FALSE);
     }
 
-    @RequestMapping(path = "/pin/bilingual", method = RequestMethod.GET)
-    public ResponseEntity<String> inviteBilingual(@RequestParam String phoneNumber,
+    @GetMapping(path = "/pin/bilingual")
+    public ResponseEntity<String> inviteBilingual(@RequestParam(value = "phoneNumber") String phoneNumber,
                                                   @RequestHeader("Session-Id") String sessionId)
-        throws NotificationClientException, UnsupportedEncodingException {
+        throws UnsupportedEncodingException {
         return getStringResponseEntity(phoneNumber, sessionId, Boolean.TRUE);
     }
 
     private ResponseEntity<String> getStringResponseEntity(String phoneNumber, String sessionId, Boolean isBilingual)
-        throws UnsupportedEncodingException, NotificationClientException {
+        throws UnsupportedEncodingException {
         if (sessionId == null) {
-            LOGGER.error("Session-Id request header not found");
+            log.error("Session-Id request header not found");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        LOGGER.info("Processing session id " + sessionId);
+        log.info("Processing session id " + sessionId);
         phoneNumber = URLDecoder.decode(phoneNumber, StandardCharsets.UTF_8.toString());
         phoneNumber = phoneNumber.replaceAll(INVALID_PHONENUMBER_CHARACTERS_REGEX, "");
         if (!phoneNumber.matches(VALID_PHONENUMBER_CHARACTERS_REGEX)) {
-            LOGGER.error("Unable to validate phoneNumber parameter");
+            log.error("Unable to validate phoneNumber parameter");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-        return ResponseEntity.ok(pinService.generateAndSend(phoneNumber, isBilingual));
+        String pin = null;
+        try {
+            pin = pinService.generateAndSend(phoneNumber, isBilingual);
+            return ResponseEntity.ok(pin);
+        } catch (NotificationClientException e) {
+            log.error("Unable to send sms with error: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
-    @RequestMapping(path = "/pin/{phoneNumber}", method = RequestMethod.GET)
+    @GetMapping(path = "/pin/{phoneNumber}")
     public String inviteLegacy(@PathVariable String phoneNumber,
                                @RequestHeader("Session-Id") String sessionId) throws NotificationClientException {
-        LOGGER.info("Processing session id " + sessionId);
+        log.info("Processing session id " + sessionId);
         return pinService.generateAndSend(phoneNumber, Boolean.FALSE);
     }
 
