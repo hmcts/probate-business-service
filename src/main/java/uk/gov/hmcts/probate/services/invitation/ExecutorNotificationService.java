ae5dbdc752ad1869b958c96fd774c7b868095137
GEvans-HMCTS <Gareth.Evans4@justice.gov.uk>
2024-08-06 10:12:07 +0100

    update controller and service with new common model

---
+++ src/main/java/uk/gov/hmcts/probate/services/invitation/ExecutorNotificationService.java
@@ -0,0 +1,64 @@
+package uk.gov.hmcts.probate.services.invitation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.probate.services.persistence.PersistenceClient;
+import uk.gov.hmcts.reform.probate.model.multiapplicant.ExecutorNotification;
+import uk.gov.hmcts.reform.probate.model.multiapplicant.Invitation;
+import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClientException;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class ExecutorNotificationService {
+
+    @Value("${services.notify.executorNotification.templateId}")
+    String templateId;
+
+    @Value("${services.notify.executorNotification.bilingualTemplateId}")
+    String bilingualTemplateId;
+
+    @Autowired
+    private PersistenceClient persistenceClient;
+
+    @Autowired
+    private NotificationClient notificationClient;
+
+    public void sendEmail(ExecutorNotification executorNotification, Boolean isBilingual)
+        throws NotificationClientException {
+        notificationClient.sendEmail(isBilingual ? bilingualTemplateId : templateId, executorNotification.getEmail(),
+            createPersonalisation(executorNotification), null);
+    }
+
+    private Map<String, String> createPersonalisation(ExecutorNotification executorNotification) {
+        HashMap<String, String> personalisation = new HashMap<>();
+
+        personalisation.put("executor_name", executorNotification.getExecutorName());
+        personalisation.put("applicant_name", executorNotification.getApplicantName());
+        personalisation.put("deceased_name", executorNotification.getDeceasedName());
+        personalisation.put("deceased_dod", executorNotification.getDeceasedDod().toString());
+        personalisation.put("ccd_reference", executorNotification.getCcdReference());
+
+        return personalisation;
+    }
+
+    public ExecutorNotification decodeURL(ExecutorNotification executorNotification) throws UnsupportedEncodingException {
+        executorNotification.setExecutorName(decodeURLParam(executorNotification.getExecutorName()));
+        executorNotification.setDeceasedName(decodeURLParam(executorNotification.getDeceasedName()));
+        executorNotification.setDeceasedDod(LocalDate.parse(decodeURLParam(executorNotification.getDeceasedDod().toString())));
+        executorNotification.setApplicantName(decodeURLParam(executorNotification.getApplicantName()));
+        executorNotification.setCcdReference(executorNotification.getCcdReference());
+        return executorNotification;
+    }
+
+    private String decodeURLParam(String uriParam) throws UnsupportedEncodingException {
+        return URLDecoder.decode(uriParam, StandardCharsets.UTF_8.toString());
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/probate/services/invitation/controllers/ExecutorNotificationController.java
@@ -0,0 +1,76 @@
+package uk.gov.hmcts.probate.services.invitation.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.probate.services.idgeneration.IdGeneratorService;
+import uk.gov.hmcts.probate.services.invitation.ExecutorNotificationService;
+import uk.gov.hmcts.reform.probate.model.multiapplicant.ExecutorNotification;
+import uk.gov.hmcts.reform.probate.model.multiapplicant.Invitation;
+import uk.gov.service.notify.NotificationClientException;
+
+import javax.validation.Valid;
+import javax.ws.rs.core.MediaType;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Tag(name = "Executor Notification Service")
+public class ExecutorNotificationController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExecutorNotificationController.class);
+    private static final String SESSION_MSG = "Processing session id {} : {}";
+
+    @Autowired
+    @Qualifier("identityGeneratorService")
+    private IdGeneratorService idGeneratorService;
+
+    @Autowired
+    private ExecutorNotificationService executorNotificationService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping(path = "/executor-notification/bilingual", consumes = MediaType.APPLICATION_JSON)
+    public String signedBilingual(@Valid @RequestBody ExecutorNotification encodedExecutorNotification,
+                                  BindingResult bindingResult,
+                                  @RequestHeader("Session-Id") String sessionId)
+        throws NotificationClientException, UnsupportedEncodingException {
+        return sendNotification(encodedExecutorNotification, bindingResult, sessionId, Boolean.TRUE);
+    }
+
+    @GetMapping(path = "/executor-notification", consumes = MediaType.APPLICATION_JSON)
+    public String signed(@Valid @RequestBody ExecutorNotification encodedExecutorNotification,
+                         BindingResult bindingResult,
+                         @RequestHeader("Session-Id") String sessionId)
+        throws NotificationClientException, UnsupportedEncodingException {
+        return sendNotification(encodedExecutorNotification, bindingResult, sessionId, Boolean.FALSE);
+    }
+
+    @GetMapping(path = "/executor-notification/all", consumes = MediaType.APPLICATION_JSON)
+    public String allSigned(@Valid @RequestBody ExecutorNotification encodedExecutorNotification,
+                         BindingResult bindingResult,
+                         @RequestHeader("Session-Id") String sessionId)
+        throws NotificationClientException, UnsupportedEncodingException {
+        return sendNotification(encodedExecutorNotification, bindingResult, sessionId, Boolean.FALSE);
+    }
+
+    private String sendNotification(ExecutorNotification encodedExecutorNotification, BindingResult bindingResult, String sessionId,
+                                  Boolean isBlingual) throws UnsupportedEncodingException, NotificationClientException {
+        LOGGER.info(SESSION_MSG, getSessionId(sessionId), bindingResult.getFieldErrors());
+        ExecutorNotification executorNotification = executorNotificationService.decodeURL(encodedExecutorNotification);
+
+        executorNotificationService.sendEmail(executorNotification, isBlingual);
+        return linkId;
+    }
+
+    private String getSessionId(String sessionId) {
+        return sessionId.replaceAll("[\n|\r|\t]", "_");
+    }
+}
