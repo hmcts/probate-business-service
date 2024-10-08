17070abb4fcf109621be660db46e527fbc714ba1
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-10-08 09:11:35 +0100

    DTSPB-4261 Initial commit of Document upload notify

---
+++ src/main/java/uk/gov/hmcts/probate/services/businessdocuments/services/DocumentNotificationService.java
@@ -0,0 +1,91 @@
+package uk.gov.hmcts.probate.services.businessdocuments.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.probate.model.documents.DocumentNotification;
+import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClientException;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class DocumentNotificationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DocumentNotificationService.class);
+
+    @Value("${services.notify.documentNotification.documentUploadedTemplateId}")
+    String documentUploadedTemplateId;
+
+    @Value("${services.notify.documentNotification.documentUploadedTemplateId}")
+    String documentUploadedBilingualTemplateId;
+
+    @Value("${services.notify.documentNotification.documentUploadIssueTemplateId}")
+    String documentUploadIssueTemplateId;
+
+    @Value("${services.notify.documentNotification.documentUploadIssueTemplateId}")
+    String documentUploadIssueBilingualTemplateId;
+
+    @Autowired
+    private NotificationClient notificationClient;
+
+    public void sendEmail(DocumentNotification encodedDocumentNotification, Boolean isBilingual) {
+        try {
+        DocumentNotification documentNotification = decodeURL(encodedDocumentNotification);
+        LOGGER.info("sending document uploaded email");
+        notificationClient.sendEmail(isBilingual ? documentUploadedBilingualTemplateId : documentUploadedTemplateId,
+            documentNotification.getEmail(), createPersonalisation(documentNotification), null);
+        } catch (NotificationClientException | UnsupportedEncodingException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+     public void sendUploadIssueEmail(DocumentNotification encodedDocumentNotification, Boolean isBilingual) {
+        try {
+            DocumentNotification documentNotification = decodeURL(encodedDocumentNotification);
+            LOGGER.info("sending document upload issue email");
+            notificationClient.sendEmail(isBilingual ? documentUploadIssueBilingualTemplateId
+                : documentUploadIssueTemplateId,
+                documentNotification.getEmail(), createPersonalisation(documentNotification), null);
+        } catch (NotificationClientException | UnsupportedEncodingException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    private Map<String, String> createPersonalisation(DocumentNotification documentNotification) {
+        HashMap<String, String> personalisation = new HashMap<>();
+
+        personalisation.put("applicant_name", documentNotification.getApplicantName());
+        personalisation.put("deceased_name", documentNotification.getDeceasedName());
+        personalisation.put("deceased_dod", documentNotification.getDeceasedDod());
+        personalisation.put("ccd_reference", documentNotification.getCcdReference());
+        personalisation.put("RESPONSE", documentNotification.getCitizenResponse());
+        personalisation.put("FILE NAMES", String.join("\n", documentNotification.getFileName()));
+        personalisation.put("UPDATE DATE", documentNotification.getCitizenResponseSubmittedDate());
+        return personalisation;
+    }
+
+    public DocumentNotification decodeURL(DocumentNotification documentNotification)
+        throws UnsupportedEncodingException {
+        documentNotification.setDeceasedName(decodeURLParam(documentNotification.getDeceasedName()));
+        documentNotification.setDeceasedDod(decodeURLParam(documentNotification.getDeceasedDod()));
+        documentNotification.setApplicantName(decodeURLParam(documentNotification.getApplicantName()));
+        documentNotification.setCcdReference(documentNotification.getCcdReference());
+        documentNotification.setEmail(documentNotification.getEmail());
+        documentNotification.setCitizenResponse(decodeURLParam(documentNotification.getCitizenResponse()));
+        documentNotification.setFileName(documentNotification.getFileName());
+        documentNotification.setCitizenResponseSubmittedDate(decodeURLParam(documentNotification
+            .getCitizenResponseSubmittedDate()));
+        return documentNotification;
+    }
+
+    private String decodeURLParam(String uriParam) throws UnsupportedEncodingException {
+        return URLDecoder.decode(uriParam, StandardCharsets.UTF_8.toString());
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/probate/services/businessdocuments/services/DocumentNotificationServiceTest.java
@@ -0,0 +1,100 @@
+package uk.gov.hmcts.probate.services.businessdocuments.services;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.probate.services.businessvalidation.util.TestUtils;
+import uk.gov.hmcts.reform.probate.model.documents.DocumentNotification;
+import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClientException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class DocumentNotificationServiceTest {
+
+    public static final String ENCODED_EXEC_NOTIFICATION = "businessdocuments/documentNotification.json";
+    public static final String EXPECTED_DECODING = "businessdocuments/expectedDecodingDocumentNotification.json";
+    public ObjectMapper objectMapper;
+
+    @Autowired
+    private TestUtils utils;
+
+    @Mock
+    private NotificationClient notificationClient;
+
+    @InjectMocks
+    private DocumentNotificationService documentNotificationService;
+     private DocumentNotification documentNotification;
+
+    @BeforeEach
+     void setUp() {
+        objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+        documentNotification = DocumentNotification.builder()
+            .ccdReference("0123-4567-8901-2345")
+            .applicantName("applicant lastname")
+            .deceasedName("firstname lastname")
+            .email("email@email.com")
+            .deceasedDod("2016-12-12")
+            .citizenResponse("response")
+            .citizenResponseSubmittedDate("12 December 2016")
+            .fileName(List.of("document.pdf")).build();
+    }
+
+    @Test
+    void testSendEmailForDocumentUploaded() throws NotificationClientException {
+        documentNotificationService.sendEmail(documentNotification, false);
+        verify(notificationClient).sendEmail(isNull(),eq(documentNotification.getEmail()), any(), isNull());
+    }
+
+    @Test
+    void testSendEmailForDocumentUploadIssue() throws NotificationClientException, UnsupportedEncodingException {
+        documentNotificationService.sendUploadIssueEmail(documentNotification, false);
+        verify(notificationClient).sendEmail(isNull(),eq(documentNotification.getEmail()), any(), isNull());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSendingNotification() throws NotificationClientException {
+        doThrow(new NotificationClientException("error"))
+            .when(notificationClient).sendEmail(isNull(), eq(documentNotification.getEmail()), any(), isNull());
+        documentNotificationService.sendEmail(documentNotification, false);
+    }
+
+    @Test
+    void testDocumentNotificationDecoding() throws Exception {
+        DocumentNotification encodedDocumentNotification =
+            objectMapper.readValue(utils.getJsonFromFile(ENCODED_EXEC_NOTIFICATION), DocumentNotification.class);
+        DocumentNotification expectedDecoding =
+            objectMapper.readValue(utils.getJsonFromFile(EXPECTED_DECODING), DocumentNotification.class);
+
+        DocumentNotification decodedDocumentNotification =
+            documentNotificationService.decodeURL(encodedDocumentNotification);
+
+        assertEquals(expectedDecoding.getApplicantName(), decodedDocumentNotification.getApplicantName());
+        assertEquals(expectedDecoding.getDeceasedName(), decodedDocumentNotification.getDeceasedName());
+        assertEquals(expectedDecoding.getDeceasedDod(), decodedDocumentNotification.getDeceasedDod());
+        assertEquals(expectedDecoding.getCcdReference(), decodedDocumentNotification.getCcdReference());
+        assertEquals(expectedDecoding.getEmail(), decodedDocumentNotification.getEmail());
+        assertEquals(expectedDecoding.getCitizenResponse(), decodedDocumentNotification.getCitizenResponse());
+        assertEquals(expectedDecoding.getFileName(), decodedDocumentNotification.getFileName());
+        assertEquals(expectedDecoding.getCitizenResponseSubmittedDate(),
+            decodedDocumentNotification.getCitizenResponseSubmittedDate());
+    }
+}
