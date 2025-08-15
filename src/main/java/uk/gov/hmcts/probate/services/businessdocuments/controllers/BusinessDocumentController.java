ba164ddca8e40936176a21af4979f8c32d38174d
iswarya.pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-08-15 14:49:34 +0100

    DTSPB-4740 Added log

--- src/main/java/uk/gov/hmcts/probate/services/businessdocuments/controllers/BusinessDocumentController.java
+++ src/main/java/uk/gov/hmcts/probate/services/businessdocuments/controllers/BusinessDocumentController.java
@@ -65,6 +65,7 @@ public class BusinessDocumentController {
                                                                 @RequestHeader("ServiceAuthorization")
                                                                     String authorization) {
         log.info("call to generateBulkScanCoverSheetPDF()");
+        log.info("Cover sheet: {}", coverSheet);
 
         byte[] bytes = pdfDocumentGenerationService.generatePdf(coverSheet, DocumentType.BULK_SCAN_COVER_SHEET);
 
