b6236f47d3aba7273276e1c57b5c8fa0b2078183
Imran <Imran.Ali@justice.gov.uk>
2025-11-17 16:51:28 +0000

    altered unit tests

--- src/test/java/uk/gov/hmcts/probate/services/invitation/UKDateFormatterTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/invitation/UKDateFormatterTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.probate.services.invitation;
 
 import org.junit.jupiter.api.Test;
-import uk.gov.hmcts.probate.services.businessdocuments.model.UKLocale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,35 +9,41 @@ public class UKDateFormatterTest {
 
     @Test
     void testFormatEnglishLocale() {
-        String result = ukDateFormatter.format("2025-10-21", UKLocale.ENGLISH);
+        String result = ukDateFormatter.format("2025-10-21", UKDateFormatter.UKLocale.ENGLISH);
         assertEquals("21st October 2025", result);
     }
 
     @Test
     void testFormatWelshLocale() {
-        String result = ukDateFormatter.format("2025-10-21", UKLocale.WELSH);
+        String result = ukDateFormatter.format("2025-10-21", UKDateFormatter.UKLocale.WELSH);
         assertEquals("21 Hydref 2025", result);
     }
 
     @Test
     void testFormatNullOrEmpty() {
-        assertEquals("", ukDateFormatter.format(null, UKLocale.ENGLISH));
-        assertEquals("", ukDateFormatter.format("", UKLocale.ENGLISH));
+        assertEquals("", ukDateFormatter.format(null, UKDateFormatter.UKLocale.ENGLISH));
+        assertEquals("", ukDateFormatter.format("", UKDateFormatter.UKLocale.ENGLISH));
     }
 
     @Test
     void testFormatInvalidDate() {
         String invalidDate = "not-a-date";
-        assertEquals(invalidDate, ukDateFormatter.format(invalidDate, UKLocale.ENGLISH));
+        assertEquals(invalidDate, ukDateFormatter.format(invalidDate, UKDateFormatter.UKLocale.ENGLISH));
     }
 
     @Test
     void testDaySuffixes() {
-        assertEquals("1st October 2025", ukDateFormatter.format("2025-10-01", UKLocale.ENGLISH));
-        assertEquals("2nd October 2025", ukDateFormatter.format("2025-10-02", UKLocale.ENGLISH));
-        assertEquals("3rd October 2025", ukDateFormatter.format("2025-10-03", UKLocale.ENGLISH));
-        assertEquals("4th October 2025", ukDateFormatter.format("2025-10-04", UKLocale.ENGLISH));
-        assertEquals("11th October 2025", ukDateFormatter.format("2025-10-11", UKLocale.ENGLISH));
-        assertEquals("13th October 2025", ukDateFormatter.format("2025-10-13", UKLocale.ENGLISH));
+        assertEquals("1st October 2025", ukDateFormatter.format("2025-10-01",
+            UKDateFormatter.UKLocale.ENGLISH));
+        assertEquals("2nd October 2025", ukDateFormatter.format("2025-10-02",
+            UKDateFormatter.UKLocale.ENGLISH));
+        assertEquals("3rd October 2025", ukDateFormatter.format("2025-10-03",
+            UKDateFormatter.UKLocale.ENGLISH));
+        assertEquals("4th October 2025", ukDateFormatter.format("2025-10-04",
+            UKDateFormatter.UKLocale.ENGLISH));
+        assertEquals("11th October 2025", ukDateFormatter.format("2025-10-11",
+            UKDateFormatter.UKLocale.ENGLISH));
+        assertEquals("13th October 2025", ukDateFormatter.format("2025-10-13",
+            UKDateFormatter.UKLocale.ENGLISH));
     }
 }
